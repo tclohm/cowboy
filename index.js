06794d178f0862fe94db62d7ebeb886257653143
Taylor Lohman <taylor.lohman.tl@gmail.com>
2023-07-05 11:18:44 -0700

    slow fib sequential

---
+++ index.js
@@ -0,0 +1,35 @@
+function fibonacci(n) {
+	// check out that readability! LOL
+	return n < 1 ? 0 : n <= 2 ? 1 : fibonacci(n - 1) + fibonacci(n - 2)
+}
+
+function fib(iteration) {
+	return new Promise( function(resolve) {
+		const start = Date.now()
+		const result = fibonacci(iteration)
+		console.log(`fib action done in: ${Date.now() - start}ms`)
+		resolve(result)
+	})
+}
+
+async function main() {
+	const start = Date.now()
+	const values = await Promise.all([
+		fib(40),
+		fib(40),
+		fib(40),
+		fib(40),
+		fib(40),
+		fib(40),
+		fib(40),
+		fib(40),
+		fib(40),
+		fib(40),
+	])
+
+	console.log("vals: ", values)
+
+	console.log(`fib done in: ${Date.now() - start}ms`)
+}
+
+main().catch(console.error)
